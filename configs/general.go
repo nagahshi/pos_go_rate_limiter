93e3cf199d9c0a98e3169c6d8725468eada7ded6
nagahshi <willian.empari@gmail.com>
2024-06-22 22:29:43 -0300

    feat: configuração de ambiente

---
+++ configs/general.go
@@ -0,0 +1,26 @@
+package configs
+
+import "github.com/spf13/viper"
+
+type Conf struct {
+}
+
+func LoadConfig(path string) (cfg *Conf, err error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, err
+}
